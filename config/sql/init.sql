05df6af71958b85942511bbf8b201684e3eca000
tangtao <412145540@qq.com>
2016-08-17 21:41:42 +0800

    init

--- config/sql/init.sql
+++ config/sql/init.sql
@@ -192,3 +192,29 @@ CREATE TABLE IF NOT EXISTS order_event (
   create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   update_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间戳'
 ) CHARACTER SET utf8mb4;
+
+
+DROP FUNCTION IF EXISTS getDistance;
+
+CREATE  FUNCTION  `getDistance`(
+   lon1 float(10,7)
+  ,lat1 float(10,7)
+  ,lon2 float(10,7)
+  ,lat2 float(10,7)
+) RETURNS double
+  begin
+    declare d double;
+    declare radius int;
+    set radius = 6378140; #假设地球为正球形，直径为6378140米
+    set d = (2*ATAN2(SQRT(SIN((lat1-lat2)*PI()/180/2)
+                          *SIN((lat1-lat2)*PI()/180/2)+
+                          COS(lat2*PI()/180)*COS(lat1*PI()/180)
+                          *SIN((lon1-lon2)*PI()/180/2)
+                          *SIN((lon1-lon2)*PI()/180/2)),
+                     SQRT(1-SIN((lat1-lat2)*PI()/180/2)
+                            *SIN((lat1-lat2)*PI()/180/2)
+                          +COS(lat2*PI()/180)*COS(lat1*PI()/180)
+                           *SIN((lon1-lon2)*PI()/180/2)
+                           *SIN((lon1-lon2)*PI()/180/2))))*radius;
+    return d;
+  end;
