d6eb542b427d9cec90b39d84a7bd9bebed7f2471
tangtao <412145540@qq.com>
2016-08-31 12:09:28 +0800

    init

--- vendor/shopapi/dao/distribution_product.go
+++ vendor/shopapi/dao/distribution_product.go
@@ -63,6 +63,9 @@ func (self *DistributionProduct) WithId(id int64) (*DistributionProduct,error) {
 func (self *DistributionProductDetail) DistributionWithMerchant(merchantId int64,appId string) ([]*DistributionProductDetail,error)  {
 	var prodList []*DistributionProductDetail
 	_,err :=db.NewSession().SelectBySql("select pt.id,pt.app_id,pt.title,pt.price,pt.dis_price,pt.flag,pt.`status`,mt.id merchant_id,mt.`name` merchant_name,pt.json,dp.csn_rate,dp.id distribution_id from merchant_prod md,merchant mt,product pt,distribution_product dp left JOIN user_distribution ud on  dp.prod_id = ud.prod_id and dp.merchant_id=? where pt.status=1 and pt.app_id=dp.app_id and pt.id = dp.prod_id and dp.merchant_id = md.id  and md.merchant_id=mt.id and pt.app_id=?",merchantId,appId).LoadStructs(&prodList)
+
+	err = FillDistributionProdImgs(appId,prodList)
+
 	return prodList,err
 }
 
