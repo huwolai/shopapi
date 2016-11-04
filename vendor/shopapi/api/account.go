385b00896c723db9306f91f5f4e59a0b80e87f69
fus <289697140@qq.com>
2016-11-04 16:46:19 +0800

    余额

--- vendor/shopapi/api/account.go
+++ vendor/shopapi/api/account.go
@@ -235,14 +235,9 @@ func AccountsGet(c *gin.Context)  {
 	if accounts!=nil{
 		var detailModel *service.AccountDetailModel
 		
-		for _,account :=range accounts  {
-			
+		for _,account :=range accounts  {			
 			detailModel,_ =service.AccountDetail(account.OpenId)
 			account.Money=float64(detailModel.Amount)/100.0
-			log.Info("余额查询")
-			log.Info(detailModel.Amount)
-			log.Info(float64(detailModel.Amount)/100.0)
-			log.Info("余额查询")
 			results = append(results,accountToA(account))
 		}
 	}
