ed33d480e042caf8533459d7cf9fc6ccc77adab1
fus <289697140@qq.com>
2016-11-22 10:46:20 +0800

    debug

--- vendor/shopapi/dao/json.go
+++ vendor/shopapi/dao/json.go
@@ -2,8 +2,13 @@ package dao
 
 import (
     "encoding/json"
+    "errors"
 )
 func  JsonToMap( s string) (map[string]interface{},error) {
+	if len(s)<1 {
+		return nil,errors.New("字符串不能为空")
+	}
+
     var result map[string]interface{}
     if err := json.Unmarshal([]byte(s), &result); err != nil {
         return nil, err
