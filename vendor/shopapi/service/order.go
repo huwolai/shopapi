b00e011cbd95e409f3b39d0e5625b37a62e64473
fus <289697140@qq.com>
2016-12-14 17:12:11 +0800

    debug

--- vendor/shopapi/service/order.go
+++ vendor/shopapi/service/order.go
@@ -661,6 +661,7 @@ func OrderPayForAccount(openId string,orderNo string,payToken string,appId strin
 	}
 	money:=account.FreezeMoney-int64(order.Price*100)
 	if money<0 {
+		log.Info("扣除不可体现金额"+money)
 		money=0
 	}	
 	err	= accountDao.AccountMinusFreezeMoneyTx(order.OpenId,money,tx)
