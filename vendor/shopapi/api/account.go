ac6f3c74463c4efb2dd176b232153e4b3305b5f7
fus <289697140@qq.com>
2016-12-21 17:41:49 +0800

    debug

--- vendor/shopapi/api/account.go
+++ vendor/shopapi/api/account.go
@@ -90,27 +90,25 @@ func LoginForSMS(c *gin.Context)  {
 		return
 	}
 	
-	/* //推送
+	//推送
 	type Getui struct  {
 		Cid 		string
 		Devicetoken	string
 	}	
 	var getui Getui
 	err = c.BindJSON(&getui)
-	if err!=nil {
-		util.ResponseError400(c.Writer,err.Error())
-		return
+	if err==nil {
+		cid			:=""
+		devicetoken :=""
+		if getui.Cid!="" {
+			cid=getui.Cid
+		}
+		if getui.Devicetoken!="" {
+			devicetoken=getui.Devicetoken
+		}	
+		service.UpdateGetui(resultMap["open_id"].(string),cid,devicetoken)
 	}
-	cid			:=""
-	devicetoken :=""
-	if getui.Cid!="" {
-		cid=getui.Cid
-	}
-	if getui.Devicetoken!="" {
-		devicetoken=getui.Devicetoken
-	}	
-	service.UpdateGetui(resultMap["open_id"].(string),cid,devicetoken)
-	//推送 **** */
+	//推送 ****
 	
 	c.JSON(http.StatusOK,resultMap)
 }
